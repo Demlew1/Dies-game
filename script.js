524a67e6b9ff4a119cea1b815469eb90616c0a5f
Demtse <demtse.yibabe@gmail.com>
2024-10-25 11:40:42 +0300

    finished rolling the dice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add("hidden");
+btnRoll.addEventListener("click", function () {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  dice.classList.remove("hidden");
+  dice.src = `dice-${diceNumber}.png`;
+});
